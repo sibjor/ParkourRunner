3e75ed03470321d9377a8fdfa11bd8c4ebec5e7d
sibjor <sixten@sibjor.se>
2025-03-24 13:55:37 +0100

    SDL_EVENT_QUIT SUCCESS if statement

--- main.cpp
+++ main.cpp
@@ -9,106 +9,97 @@
  * This code is public domain. Feel free to use it for any purpose!
  */
 
- #define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
- #include <SDL3/SDL.h>
- #include <SDL3/SDL_main.h>
- 
- /* We will use this renderer to draw into this window every frame. */
- static SDL_Window *window = NULL;
- static SDL_Renderer *renderer = NULL;
- static SDL_Camera *camera = NULL;
- static SDL_Texture *texture = NULL;
- 
- 
- /* This function runs once at startup. */
- SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
- {
-     SDL_CameraID *devices = NULL;
-     int devcount = 0;
- 
-     SDL_SetAppMetadata("Example Camera Read and Draw", "1.0", "com.example.camera-read-and-draw");
- 
-     if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_CAMERA)) {
-         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
-         return SDL_APP_FAILURE;
-     }
- 
-     if (!SDL_CreateWindowAndRenderer("examples/camera/read-and-draw", 640, 480, 0, &window, &renderer)) {
-         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
-         return SDL_APP_FAILURE;
-     }
- 
-     devices = SDL_GetCameras(&devcount);
-     if (devices == NULL) {
-         SDL_Log("Couldn't enumerate camera devices: %s", SDL_GetError());
-         return SDL_APP_FAILURE;
-     } else if (devcount == 0) {
-         SDL_Log("Couldn't find any camera devices! Please connect a camera and try again.");
-         return SDL_APP_FAILURE;
-     }
- 
-     camera = SDL_OpenCamera(devices[0], NULL);  // just take the first thing we see in any format it wants.
-     SDL_free(devices);
-     if (camera == NULL) {
-         SDL_Log("Couldn't open camera: %s", SDL_GetError());
-         return SDL_APP_FAILURE;
-     }
- 
-     return SDL_APP_CONTINUE;  /* carry on with the program! */
- }
- 
- /* This function runs when a new event (mouse input, keypresses, etc) occurs. */
- SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
- {
-     if (event->type == SDL_EVENT_QUIT) {
-         return SDL_APP_SUCCESS;  /* end the program, reporting success to the OS. */
-     } else if (event->type == SDL_EVENT_CAMERA_DEVICE_APPROVED) {
-         SDL_Log("Camera use approved by user!");
-     } else if (event->type == SDL_EVENT_CAMERA_DEVICE_DENIED) {
-         SDL_Log("Camera use denied by user!");
-         return SDL_APP_FAILURE;
-     }
-     return SDL_APP_CONTINUE;  /* carry on with the program! */
- }
- 
- /* This function runs once per frame, and is the heart of the program. */
- SDL_AppResult SDL_AppIterate(void *appstate)
- {
-     Uint64 timestampNS = 0;
-     SDL_Surface *frame = SDL_AcquireCameraFrame(camera, &timestampNS);
- 
-     if (frame != NULL) {
-         /* Some platforms (like Emscripten) don't know _what_ the camera offers
-            until the user gives permission, so we build the texture and resize
-            the window when we get a first frame from the camera. */
-         if (!texture) {
-             SDL_SetWindowSize(window, frame->w, frame->h);  /* Resize the window to match */
-             texture = SDL_CreateTexture(renderer, frame->format, SDL_TEXTUREACCESS_STREAMING, frame->w, frame->h);
-         }
- 
-         if (texture) {
-             SDL_UpdateTexture(texture, NULL, frame->pixels, frame->pitch);
-         }
- 
-         SDL_ReleaseCameraFrame(camera, frame);
-     }
- 
-     SDL_SetRenderDrawColor(renderer, 0x99, 0x99, 0x99, SDL_ALPHA_OPAQUE);
-     SDL_RenderClear(renderer);
-     if (texture) {  /* draw the latest camera frame, if available. */
-         SDL_RenderTexture(renderer, texture, NULL, NULL);
-     }
-     SDL_RenderPresent(renderer);
- 
-     return SDL_APP_CONTINUE;  /* carry on with the program! */
- }
- 
- /* This function runs once at shutdown. */
- void SDL_AppQuit(void *appstate, SDL_AppResult result)
- {
-     SDL_CloseCamera(camera);
-     SDL_DestroyTexture(texture);
-     /* SDL will clean up the window/renderer for us. */
- }
- 
- 
+#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
+#define WINDOW_WIDTH 1280
+#define WINDOW_HEIGHT 960
+
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <vector>
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+/* We will use this renderer to draw into this window every frame. */
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+std::vector<SDL_Surface *> spriteSheets;
+
+bool LoadSpritePath(std::string path)
+{
+
+    if (!fs::exists(path))
+    {
+        SDL_Log("Couldn't find sprite sheets");
+        return false;
+    }
+
+    for (const auto &entry : fs::directory_iterator(path))
+    {
+        if (entry.is_regular_file())
+        {
+            const std::string filePath = entry.path().string();
+            SDL_Surface *spriteSheet = IMG_Load(filePath.c_str());
+            if (spriteSheet == NULL)
+            {
+                SDL_Log("Couldn't load sprite sheet: %s", SDL_GetError());
+                return false;
+            }
+            spriteSheets.push_back(spriteSheet);
+        }
+    }
+
+    return true;
+}
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_CAMERA))
+    {
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!SDL_CreateWindowAndRenderer("sibjor@github.com", WINDOW_WIDTH, WINDOW_HEIGHT, 0, &window, &renderer))
+    {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    if (!LoadSpritePath("assets/Basic movement pack/SpriteSheet"))
+    {
+        return SDL_APP_FAILURE;
+    }
+    if (!LoadSpritePath("assets/Basic vault pack/SpriteSheet")){
+        return SDL_APP_FAILURE;
+    }
+
+    return SDL_APP_CONTINUE; /* carry on with the program! */
+}
+
+/* This function runs when a new event (mouse input, keypresses, etc) occurs. */
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if(event->type == SDL_EVENT_QUIT){
+        return SDL_APP_SUCCESS;
+    }
+
+    return SDL_APP_CONTINUE; /* carry on with the program! */
+}
+
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+
+    return SDL_APP_CONTINUE; /* carry on with the program! */
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+}
