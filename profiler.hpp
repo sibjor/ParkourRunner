aa6b8463c3a14ada09a68be7dd09e8cc545e11a3
sibjor <sixten@sibjor.se>
2025-04-08 12:10:00 +0200

    base laid for the profiler, now lets start source file

--- profiler.hpp
+++ profiler.hpp
@@ -14,21 +14,57 @@ enum class MemoryUnit
     TERABYTE,
 };
 
+enum class TimeUnit
+{
+    NANOSECOND,
+    MICROSECOND,
+    MILLISECOND,
+    SECOND,
+    MINUTE,
+    HOUR,
+};
+
 class CPUProfiler
 {
 public:
-
+    CPUProfiler();
+    ~CPUProfiler();
+    void GetCPUInfo();
+    void GetCPUUsage();
+    void GetCPUFrequency();
+    void GetCPUTemperature();
+    void GetCPUArchitecture();
+    void GetCPUModel();
+    void GetCPUCores();
     
 };
 class MemoryProfiler
 {
 public:
+    MemoryProfiler();
+    ~MemoryProfiler();
+    void GetMemoryInfo();
+    void GetMemoryUsage();
+    void GetMemorySpeed();
+    void GetMemoryType();
+    void GetMemorySize(MemoryUnit unit);
+    void GetMemoryFreeSpace(MemoryUnit unit);
+    void GetMemoryUsedSpace(MemoryUnit unit);
 
     
 };
 class DiskProfiler
 {
 public:
+    DiskProfiler();
+    ~DiskProfiler();
+    void GetDiskInfo();
+    void GetDiskUsage();
+    void GetDiskSpeed();
+    void GetDiskType();
+    void GetDiskSize(MemoryUnit unit);
+    void GetDiskFreeSpace(MemoryUnit unit);
+    void GetDiskUsedSpace(MemoryUnit unit);
 
     
 };
@@ -37,6 +73,13 @@ class TimeProfiler
 public:
     TimeProfiler();
     ~TimeProfiler();
+
+    void GetCurrentTime();
+    void GetCurrentDate();
+    void GetCurrentDateTime();
+    void StartTimer();
+    void StopTimer();
+    void GetElapsedTime(TimeUnit unit);
     
 private:
     std::unique_ptr<Time> time;
