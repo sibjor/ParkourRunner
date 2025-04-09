cfe05171945eeedf597623930467c67c6bf169c5
sibjor <sixten@sibjor.se>
2025-04-09 13:50:59 +0200

    sdl headers

--- construct.cpp
+++ construct.cpp
@@ -1,1 +1,9 @@
 #include "construct.hpp"
+
+void SDLMainLoop::InitializeSDL()
+{
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL_Init(SDL_INIT_VIDEO) failed: %s\n", SDL_GetError());
+        return(2);
+    }
+}
--- construct.hpp
+++ construct.hpp
@@ -7,7 +7,10 @@
 
 #include "process.hpp"
 
-#define SDL_MAIN_USE_CALLBACKS 1  /* use the callbacks instead of main() */
+#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
+
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
 
 enum class Type
 {
@@ -23,3 +26,28 @@ enum class Type
 
 };
 
+class SDLMainLoop
+{
+
+public:
+    SDLMainLoop();
+    ~SDLMainLoop();
+    SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]);
+    SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event);
+    SDL_AppResult SDL_AppIterate(void *appstate);
+    void SDL_AppQuit(void *appstate, SDL_AppResult result);
+
+public:
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+};
+
+class Construct : public Process, public FileManager, public Profiler
+{
+public:
+    Construct(const char *name_of_host, const char *name_of_self, const u_int *id);
+    ~Construct();
+    void InitializeSDL();
+    void InititalizeProfiler();
+    void InitializeFileSystem();
+};
