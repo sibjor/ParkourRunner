eadfff98620d9d31dbb5569c908c2dd1843d62ec
sibjor <sixteen610@gmail.com>
2025-04-15 12:40:48 +0200

    New way of methodical means, updated TODO structure as well

--- TODO.md
+++ TODO.md
@@ -1,17 +1,18 @@
 # Todo List for Parkour Runner
 
-## TODO List of April 15
-[ ] Move character up and down
-[ ] Move character left and right
-[ ] Implement collision and distance detection
-[ ] Refactor animation logic
+## NOT DONE list
+[ ] Render and Size Ground
+[ ] Render and Size Player
+[ ] Render and Size All Obstacles
+[ ] Maneuver Player
+[ ] Profile Program
+
+
+## DONE list
 [x] New FRect objects for each rendered item
 [x] Game objects/obstacles matching animations
 [x] Refactor PlayAnimation the correctly loop (or not) animation
 [x] Quit button
-
-
-## First TODO list
 [x] Verify SDL_GetTicks()
 [x] Refactor loose element into dedicated classes
 [x] Implement similar the the profiling method stated above, but in vanilla C23
@@ -25,10 +26,4 @@
 [x] Load SpriteSheets as textures in vector
 [x] Slice whole indexes of the [Texture Vector(s)](https://www.reddit.com/r/sdl/comments/1bo7k1l/tutorial_for_spritestrips/?rdt=46632). 
 [x] Animate a strip from a sheet - (use SDL_GetTicks / 100 for 1/10 of a second?)
-[ ] Implement floor/ground level with aseprite or SDL_Renderer
-[ ] Implement obstacles with Aseprite or SDL_Renderer
 [x] (Halfway) Implement callback events (read keyboard input)
-[ ] Move obstacles in relation to player movement (sidescrolling)
-[ ] Procedural generation of obstacles
-[ ] (Optional) Implement Background Artwork
-[ ] (Optional) Implement controller input (handheld)
--- main.cpp
+++ main.cpp
@@ -1,1 +1,61 @@
 #include "render.hpp"
+
+/*
+  Copyright (C) 1997-2025 Sam Lantinga <slouken@libsdl.org>
+
+  This software is provided 'as-is', without any express or implied
+  warranty.  In no event will the authors be held liable for any damages
+  arising from the use of this software.
+
+  Permission is granted to anyone to use this software for any purpose,
+  including commercial applications, and to alter it and redistribute it
+  freely.
+*/
+#define SDL_MAIN_USE_CALLBACKS 1 /* use the callbacks instead of main() */
+
+static SDL_Window *window;
+static SDL_Renderer *renderer;
+const char *window_title = "Validator 13";
+const int window_width = 1000;
+const int window_height = 1000;
+const int window_flags = SDL_EVENT_WINDOW_SHOWN;
+
+/* This function runs once at startup. */
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
+{
+    /* Create the window */
+    if (!SDL_CreateWindowAndRenderer(window_title, window_width, window_height, window_flags, &window, &renderer))
+    {
+        SDL_Log("Couldn't create window and renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_KEY_DOWN ||
+        event->type == SDL_EVENT_QUIT)
+    {
+        return SDL_APP_SUCCESS; /* end the program, reporting success to the OS. */
+    }
+    return SDL_APP_CONTINUE;
+}
+
+/* This function runs once per frame, and is the heart of the program. */
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    /* Set the background color to white */
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderClear(renderer);
+
+    /* Present the renderer */
+    SDL_RenderPresent(renderer);
+
+    return SDL_APP_CONTINUE;
+}
+
+/* This function runs once at shutdown. */
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+}
